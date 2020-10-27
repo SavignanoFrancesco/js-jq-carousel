9624afd01dcedf26e716cfdc9b40da2f9a486d9d
Francesco <francesco.savignano96@gmail.com>
2020-10-27 16:33:43 +0100

    scorrimento delle immagini verso sinistra impostato

--- main.js
+++ main.js
@@ -20,4 +20,24 @@ $( document ).ready(function() {
 
     });
 
+    $('.prev i').click(function() {
+
+        //salvo l'immagine corrente in una variabile
+        var current_img = $('img.active');
+
+        //rimuovo la classe active dall'immagine corrente
+        current_img.removeClass('active');
+
+        //se esiste un immagine precedente all'immagine corrente
+        if(current_img.prev().length){
+            //aggiungo classe active all'immagine precedente
+            current_img.prev().addClass('active');
+        //altrimenti
+        }else{
+            //aggiungo la classe active all'ultima' immagine del blocco slides
+            $('.slides img:last-of-type').addClass('active');
+        }
+
+    });
+
 });
