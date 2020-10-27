7a6dcc50126888a88afdc4453b94c8817b7ac497
Francesco <francesco.savignano96@gmail.com>
2020-10-27 16:30:26 +0100

    scorrimento delle immagini verso destra impostato

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 $( document ).ready(function() {
-    
+
+    $('.next i').click(function() {
+
+        //salvo l'immagine corrente in una variabile
+        var current_img = $('img.active');
+
+        //rimuovo la classe active dall'immagine corrente
+        current_img.removeClass('active');
+
+        //se esiste un immagine dopo l'immagine corrente
+        if(current_img.next().length){
+            //aggiungo classe active all'immagine successiva
+            current_img.next().addClass('active');
+        //altrimenti
+        }else{
+            //aggiungo la classe active alla prima immagine del blocco slides
+            $('.slides img:first-of-type').addClass('active');
+        }
+
+    });
+
 });
