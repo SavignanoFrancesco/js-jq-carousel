ea35e327b6835913e5a2e9b93635c30e17a2f2d0
Francesco <francesco.savignano96@gmail.com>
2020-10-27 17:11:21 +0100

    scorrimento dei bullets verso sinistra impostato

--- main.js
+++ main.js
@@ -33,18 +33,27 @@ $( document ).ready(function() {
 
         //salvo l'immagine corrente in una variabile
         var current_img = $('img.active');
+        //salvo il bullet corrente in una variabile
+        var current_bullet = $('.bullets i.fas');
 
         //rimuovo la classe active dall'immagine corrente
         current_img.removeClass('active');
+        //svuoto il bullet corrente(fas = bullet pieno, far = bullet vuoto)
+        current_bullet.removeClass('fas');
+        current_bullet.addClass('far');
 
         //se esiste un immagine precedente all'immagine corrente
         if(current_img.prev('img').length){
             //aggiungo classe active all'immagine precedente
             current_img.prev('img').addClass('active');
+            //riempio il bullet precedente
+            current_bullet.prev('i').addClass('fas');
         //altrimenti
         }else{
             //aggiungo la classe active all'ultima immagine del blocco slides
             $('.slides img:last-of-type').addClass('active');
+            //riempio l'ultimo bullet
+            $('.bullets i:last-of-type').addClass('fas');
         }
 
     });
