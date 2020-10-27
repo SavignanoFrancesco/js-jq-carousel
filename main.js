185c6c64722b00e44df899ed455321b76f82ebfd
Francesco <francesco.savignano96@gmail.com>
2020-10-27 17:08:40 +0100

    scorrimento dei bullet verso destra impostato

--- main.js
+++ main.js
@@ -4,18 +4,27 @@ $( document ).ready(function() {
 
         //salvo l'immagine corrente in una variabile
         var current_img = $('img.active');
+        //salvo il bullet corrente in una variabile
+        var current_bullet = $('.bullets i.fas');
 
         //rimuovo la classe active dall'immagine corrente
         current_img.removeClass('active');
+        //svuoto il bullet corrente(fas = bullet pieno, far = bullet vuoto)
+        current_bullet.removeClass('fas');
+        current_bullet.addClass('far');
 
         //se esiste un immagine dopo l'immagine corrente
-        if(current_img.next().length){
+        if(current_img.next('img').length){
             //aggiungo classe active all'immagine successiva
-            current_img.next().addClass('active');
+            current_img.next('img').addClass('active');
+            //riempio il bullet successivo
+            current_bullet.next('i').addClass('fas');
         //altrimenti
         }else{
             //aggiungo la classe active alla prima immagine del blocco slides
             $('.slides img:first-of-type').addClass('active');
+            //riempio il primo bullet
+            $('.bullets i:first-of-type').addClass('fas');
         }
 
     });
@@ -29,9 +38,9 @@ $( document ).ready(function() {
         current_img.removeClass('active');
 
         //se esiste un immagine precedente all'immagine corrente
-        if(current_img.prev().length){
+        if(current_img.prev('img').length){
             //aggiungo classe active all'immagine precedente
-            current_img.prev().addClass('active');
+            current_img.prev('img').addClass('active');
         //altrimenti
         }else{
             //aggiungo la classe active all'ultima immagine del blocco slides
